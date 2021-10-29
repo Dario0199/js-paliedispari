d79387296cf89729b9efd11e73c5f27a4294e0fe
Dario0122 <dario.di.cuia@gmail.com>
2021-10-29 17:20:44 +0200

    pari e dispari 2

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -5,6 +5,7 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+//utente sceglie pari o dispari e un numero da 1 a 5
 const btnEven = document.querySelector('.btn-even')
 btnEven.addEventListener('click', function(){
     const number1 = parseInt( prompt('Inserisci un numero da 1 a 5') );
@@ -14,28 +15,29 @@ btnEven.addEventListener('click', function(){
         if(number1 >= 1 || number1 <= 5){
         }
     }
-
+    //generiamo un numero random per il computer
     const randNum = getRandomNumber(1, 5)
     
     function getRandomNumber(min, max){
         const rand = Math.floor( Math.random()* (max - min + 1)) + min;
         return rand;
     }
-
+    //somma del numero utente e del numero random
     const som = number1 + randNum;
-    
+    //riconosciamo se il numero è pari o dispari
     const resNumber = numEvenOdd(som, 2)
     
     function numEvenOdd(somma, divisore){
         const res = somma % divisore === 0;
         return res;
     }
-    
+    //risultato
     if(resNumber == true){
         console.log('Hai vinto!!!');
     } else {
         console.log('Hai perso :(');
     }
+    
     console.log(`Il tuo numero è ${number1}`);
     console.log(`il numero random è ${randNum}`)
     console.log(`La somma dei due numeri è ${som}`);
