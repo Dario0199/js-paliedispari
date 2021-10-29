f416ac7f2c1d235ec05a64f51b71cadbfcde9352
Dario0122 <dario.di.cuia@gmail.com>
2021-10-29 15:50:50 +0200

    palindroma 2

--- palindroma/main.js
+++ palindroma/main.js
@@ -3,18 +3,21 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 const wordUser = prompt('Inserisci una parola');
-console.log(wordUser)
 
 const wordReverse = reverseChars(wordUser);
-console.log(wordReverse)
 
 function reverseChars(word){
     let reverse = '';
-    
-    for(let i = word.lenght - 1; i >= 0; i--){
-        console.log( word[i]);
+    console.log(word);
+    for(let i = word.length - 1; i >= 0; i--){
         reverse += word[i]
     }
 
     return reverse;
+}
+
+if(wordUser === wordReverse){
+    console.log('La parola inserita è palindroma');
+} else {
+    console.log('La parola inserita non è palindroma');
 }
